cca0eb3b259d7dc7654c87d90cf9cf9898b69f3e
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-08-07 16:31:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    // debugger;
+    let newArray = [];
+    for (let i=0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return newArray;
+};
+
+function countDown(i) {
+    while (i>=0) {
+        console.log(i);
+        i--;
+        
+    };
+};
